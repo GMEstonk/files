7f5bad7e30a190ad2c2c48ac35b55c5e4f6ac6ad
GMEstonk <112446331+GMEstonk@users.noreply.github.com>
2023-06-13 17:55:44 -0500

    Update xhr-redirect.js

--- xhr-redirect.js
+++ xhr-redirect.js
@@ -1,6 +1,6 @@
 
 let rH = window.location.host.replace('.servleteer.com','');
-
+if(!XMLHttpRequest.nativeOpen){
 XMLHttpRequest.prototype.nativeOpen = XMLHttpRequest.prototype.open;
 
 XMLHttpRequest.prototype.customOpen = function(method, url, asynch, user, password) {
@@ -19,3 +19,29 @@ XMLHttpRequest.prototype.customOpen = function(method, url, asynch, user, passwo
 }
 
 XMLHttpRequest.prototype.open = XMLHttpRequest.prototype.customOpen;
+
+
+
+/*//////////////////////////////////////////////////////////////////////////*/
+
+
+XMLHttpRequest.nativeOpen = XMLHttpRequest.open;
+
+XMLHttpRequest.customOpen = function(method, url, asynch, user, password) {
+  url = url.replaceAll(rH, window.location.host);
+  url=url.replace('gql.reddit.com/','gql.reddit.com.servleteer.com/');
+  url=url.replaceAll('.servleteer.com.servleteer.com','.servleteer.com');
+  this.method = method;
+  this.requestURL = url;
+  this.asynch = asynch;
+  if (user) { this.user = user; }
+  if (password) { this.password = password; }
+  this.requestHeaders = new Map();
+
+  return this.nativeOpen(method, url, asynch, user, password);
+
+}
+
+XMLHttpRequest.open = XMLHttpRequest.customOpen;
+  
+}
