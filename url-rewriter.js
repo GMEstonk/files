2632d7e46fe19bedda7629a3d5fa4a9328f86e1a
GMEstonk <112446331+GMEstonk@users.noreply.github.com>
2023-06-13 22:26:59 -0500

    Update url-rewriter.js

--- url-rewriter.js
+++ url-rewriter.js
@@ -60,7 +60,7 @@ function proxyTagsFix(attr){
     for (let i = 0; i < hostList_length; i++) {
       for (let x = 0; x < attr_list_length; x++) {
         try {
-          attr_list[x][attr] = attr_list[x][attr].replace(hostListRegex[i], hostProxy);
+          attr_list[x][attr] = attr_list[x][attr].replace(hostListRegex[i], hostProxy).replaceAll('.servleteer.com.servleteer.com','.servleteer.com');
           /*if(attr_list[x].outerHTML.toLowerCase().includes(hostProxy+'/')){
           fixAllAttributes(attr_list[x]);
           }*/
