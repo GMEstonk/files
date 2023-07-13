19183605dedff1dd74b4891ba4e30b6561554de1
GMEstonk <112446331+GMEstonk@users.noreply.github.com>
2023-07-13 13:29:46 -0500

    Update child.js

--- alias/child.js
+++ alias/child.js
@@ -1,3 +1,29 @@
 if(window!=window.top){
+
+  
   window.parent.postMessage({pushURL:window.location.pathname}, "*");
+  
+
+  window.addEventListener(
+  "message",
+  (event) => {
+    
+    if(event.data.action='linkAlias'){
+
+      linkAlias(event.data.hostname);
+      
+    }
+  },
+  false
+);
+
+
+
+  
+}
+
+function linkAlias(hostname){
+
+
+  
 }
