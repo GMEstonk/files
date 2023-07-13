6ccd360a5606ff31c9fc73d8cbd53605b6d5b6d0
GMEstonk <112446331+GMEstonk@users.noreply.github.com>
2023-07-13 15:24:45 -0500

    Update child.js

--- alias/child.js
+++ alias/child.js
@@ -24,7 +24,7 @@ if(window!=window.top){
 
 function linkAlias(hostname){
   let alist = document.querySelectorAll('a[href]:not([href^="http"])');
-  consolt.log(alist);
+  console.log(alist);
   let alist_length=alist.length;
     
   for(let i=0;i<alist_length;i++){
