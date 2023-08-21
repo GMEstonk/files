bec551296b6490aac286b4c3dcaf9a8e283e12d6
GMEstonk <112446331+GMEstonk@users.noreply.github.com>
2023-08-21 09:04:30 -0500

    Update lengua-client.js

--- lenguapedia.org/lengua-client.js
+++ lenguapedia.org/lengua-client.js
@@ -1,1 +1,16 @@
+function textNodesUnder(el){
+  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+  while(n=walk.nextNode()) a.push(n);
+  return a;
+}
 
+document.addEventListener("DOMContentLoaded", (event) => {
+
+let textNodes = textNodesUnder(document.querySelector('main'));
+const textNodes_length=textNodes.length;
+  for(let i=0;i<textNodes_lengthl;i++){try{
+    textNodes[i].textContent=textNodes[i].replaceAll('Wiki','Lengua');
+  }catch(e){continue;}}
+
+  
+});
